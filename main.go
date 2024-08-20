4ddc4d5064f40f3cbb3998ae57483f7cc0c7b3e5
Alec Draymore <draymore26@gmail.com>
2024-08-20 18:53:28 -0400

    Add .env support

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	println("Hello Top Spot")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 }
